946bba837d9b4355a747d43866aac723c7ab99aa
Hoàng Khang <vuhoangkhang19032000kg@gmail.com>
2024-07-23 02:41:37 +0700

    Up

---
+++ Backend/database/sqlite.js
@@ -0,0 +1,29 @@
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+
+// Ensure the path is correct
+const dbPath = path.resolve(__dirname, 'blockchain.db');
+
+// Open the database
+let db = new sqlite3.Database(dbPath, (err) => {
+  if (err) {
+    console.error('Error opening database:', err.message);
+  } else {
+    console.log('Connected to the SQLite database.');
+  }
+});
+
+db.serialize(() => {
+    db.run("CREATE TABLE lorem (info TEXT)");
+
+    const stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+    for (let i = 0; i < 10; i++) {
+        stmt.run("Ipsum " + i);
+    }
+    stmt.finalize();
+
+    db.each("SELECT rowid AS id, info FROM lorem", (err, row) => {
+        console.log(row.id + ": " + row.info);
+    });
+});
+db.close();
